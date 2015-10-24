fa73a684d68325e86541d06e27e70d2af50d03bc
dylandy.chang <dylandy.chang@gmail.com>
2015-10-24 15:56:15 +0800

    added jquery and normalise with basic css and icon font added

---
+++ public/config.rb
@@ -0,0 +1,12 @@
+
+css_dir = "css" # by Fire.app 
+sass_dir = "scss" # by Fire.app 
+images_dir = "img" # by Fire.app 
+javascripts_dir = "js" # by Fire.app 
+output_style = :compressed # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+sourcemap = false # by Fire.app 
+fireapp_coffeescript_options = {:bare=>true} # by Fire.app 
+fireapp_livescript_options = {:bare=>true} # by Fire.app 
